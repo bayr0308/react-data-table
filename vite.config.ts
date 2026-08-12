9bee78ff58273d7b4d54c078180a1e5f0ecc74ab
Bair <bayr0308@yandex.ru>
2026-08-12 16:57:42 +0300

    build: add deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 const viteConfig = defineConfig({
+  base: "/react-data-table/",
   plugins: [react(), tailwindcss()],
 });
 
